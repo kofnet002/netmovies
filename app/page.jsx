7ab4430828c23031b54ad96a8ac34f29247da0d4
frimpong-kofi <noeljoel61@gmail.com>
2023-04-22 20:00:29 +0000

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className='text-lg py-5 mx-5'>Hello world</h1>
+    </main>
+  )
+}
